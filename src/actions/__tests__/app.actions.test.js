178fb59892833cbf80efdf563a90b24eccb75eaa
Paul Guerra <paulrguerra@gmail.com>
2018-03-19 09:02:15 -0400

    start tests

---
+++ src/actions/__tests__/app.actions.test.js
@@ -0,0 +1,10 @@
+/* global describe, it, expect */
+import ready from '../app.actions';
+import ACTIONS from '../action.types';
+
+describe('App Ready', () => {
+  it('dispatches a ready action', () =>{
+    let action = ready();
+    expect(action.type).toEqual(ACTIONS.APP.READY);
+  });
+});
